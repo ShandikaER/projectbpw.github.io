b6385bf59d59d4ee15e7e58f7db4a86d479f0bc6
ShandikaER <sandika805@gmail.com>
2022-07-21 00:03:09 +0700

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://projectbpw.github.io.test/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Kategori extends CI_Controller {
+class Kategori extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -18,32 +19,41 @@ class Kategori extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
-	function __construct() {
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model('M_kategori');
 	}
-    public function whitening(){
-        $data['whitening'] = $this->M_kategori->data_whitening()->result();
-        $this->load->view('templates/header');
-		$this->load->view('whitening', $data);
+	public function index()
+	{
+		var_dump("test");
+	}
+	public function whitening()
+	{
+		$data['whitening'] = $this->M_kategori->data_whitening()->result();
+		$this->load->view('templates/header');
+		$this->load->view('kategori/whitening', $data);
 		$this->load->view('templates/footer');
-    }
-    public function acne(){
-        $data['acne'] = $this->M_kategori->data_acne()->result();
-        $this->load->view('templates/header');
-		$this->load->view('acne', $data);
+	}
+	public function acne()
+	{
+		$data['acne'] = $this->M_kategori->data_acne()->result();
+		$this->load->view('templates/header');
+		$this->load->view('kategori/acne', $data);
 		$this->load->view('templates/footer');
-    }
-    public function ultimate(){
-        $data['ultimate'] = $this->M_kategori->data_ultimate()->result();
-        $this->load->view('templates/header');
-		$this->load->view('ultimate', $data);
+	}
+	public function ultimate()
+	{
+		$data['ultimate'] = $this->M_kategori->data_ultimate()->result();
+		$this->load->view('templates/header');
+		$this->load->view('kategori/ultimate', $data);
 		$this->load->view('templates/footer');
-    }   
-    public function luminous(){
-        $data['luminous'] = $this->M_kategori->data_luminous()->result();
-        $this->load->view('templates/header');
-		$this->load->view('luminous', $data);
+	}
+	public function luminous()
+	{
+		$data['luminous'] = $this->M_kategori->data_luminous()->result();
+		$this->load->view('templates/header');
+		$this->load->view('kategori/luminous', $data);
 		$this->load->view('templates/footer');
-    }   
+	}
 }
